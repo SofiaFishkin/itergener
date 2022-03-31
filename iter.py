00d17b6009e4e188248aefdb09a030a9aed6c81b
Rachel Fishkin <rachelfishkin@MacBook-Pro-Rachel.local>
2022-04-01 01:48:33 +0300

    third commit

--- iter.py
+++ iter.py
@@ -1,17 +1,6 @@
-
-from itertools import chain
-from typing import Collection
-
-nested_list =[
- ['a', 'b', 'c'],
- ['d', 'e', 'f'],
- ['g', 'h', 'i'],
-]
-nested_list=list(chain(* nested_list))
-
 class Iterator:
-    def __init__(self, collection):
-        self.collection = collection
+    def __init__(self, nested_list):
+        self.collection = nested_list
     
     def __iter__(self):
         self.cursor = -1
@@ -23,6 +12,12 @@ class Iterator:
             raise StopIteration
         element = self.collection[self.cursor]
         return element
+
+nested_list =[
+ ['a', 'b', 'c'],
+ ['d', 'e', 'f'],
+ ['g', 'h', 'i'],
+]
 
 for item in Iterator(nested_list):
     print(item)
