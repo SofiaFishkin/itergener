ad2d16b2b334acd1a3bc279b18863fad2a216adc
Rachel Fishkin <rachelfishkin@MacBook-Pro-Rachel.local>
2022-04-13 21:20:42 +0300

    million commit

--- iter.py
+++ iter.py
@@ -1,23 +1,35 @@
-class Iterator:
-    def __init__(self, nested_list):
-        self.collection = nested_list
-    
+class FlatIterator:
+    def __init__(self, collection) -> None:
+        self.collection = collection
+        self.current = 0
+        self.inner_current = 0
+
     def __iter__(self):
-        self.cursor = -1
         return self
 
     def __next__(self):
-        self.cursor += 1
-        if self.cursor == len(self.collection):
+        try:
+            if self.current <= len(self.collection):
+                next_list = self.collection[self.current]
+                if self.inner_current == len(next_list):
+                    self.inner_current = 0
+                    self.current += 1
+                next_val = self.collection[self.current][self.inner_current]
+                self.inner_current += 1
+                return next_val
+        except:
             raise StopIteration
-        element = self.collection[self.cursor]
-        return element
 
-nested_list =[
- ['a', 'b', 'c'],
- ['d', 'e', 'f'],
- ['g', 'h', 'i'],
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
 ]
 
-for item in Iterator(nested_list):
-    print(item)
+
+for item in FlatIterator(nested_list):
+    print(item) #
+
+
+print([v for v in FlatIterator(nested_list)])
+
